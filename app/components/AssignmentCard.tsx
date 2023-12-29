d9aaa13ed151804b97a46413cab816f5fb60c110
JokerSA <asatishubham2004@gmail.com>
2023-12-29 09:53:55 +0530

    modal created

--- app/components/AssignmentCard.tsx
+++ app/components/AssignmentCard.tsx
@@ -1,9 +1,10 @@
-"use client";
+"use client"
+import { useState } from "react";
 import Link from "next/link";
 
 interface AssignmentCardProps {
   assignmentTitle: string;
-  tags?: string[]; // Make tags optional
+  tags?: string[];
   assignmentDescription: string;
   dueDate: string;
   assignmentId: string;
@@ -11,11 +12,21 @@ interface AssignmentCardProps {
 
 const AssignmentCard = ({
   assignmentTitle,
-  tags = [], // Provide a default empty array
+  tags = [],
   assignmentDescription,
   dueDate,
   assignmentId,
 }: AssignmentCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="bg-white bg-opacity-10 shadow-lg rounded-md overflow-hidden">
       <div className="p-4">
@@ -24,10 +35,7 @@ const AssignmentCard = ({
         </h2>
         <div className="flex flex-wrap gap-2 mb-4">
           {tags.map((tag, index) => (
-            <span
-              key={index}
-              className="bg-white text-black px-2 py-1 rounded"
-            >
+            <span key={index} className="bg-white text-black px-2 py-1 rounded">
               {tag}
             </span>
           ))}
@@ -35,20 +43,46 @@ const AssignmentCard = ({
         <p className="text-white mb-4">{assignmentDescription}</p>
         <p className="text-white">Due Date: {dueDate}</p>
         <div className="mt-4 flex justify-between items-center">
-          <Link
-            href={`/assignments/${assignmentId}`}
+          <button
+            onClick={openModal}
             className="bg-blue-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-600"
           >
             More Details
-          </Link>
+          </button>
           <Link
-            href={`/submit/${assignmentId}`}
+            href={`/assignments/submit/${assignmentId}`}
             className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
           >
             Submit Assignment
           </Link>
         </div>
       </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 px-3 flex items-center justify-center">
+          <div className="bg-white p-6 rounded-md">
+            <h2 className="text-2xl font-semibold mb-2">{assignmentTitle}</h2>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="bg-blue-500 text-white px-2 py-1 rounded"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <p className="text-gray-700 mb-4">{assignmentDescription}</p>
+            <p className="text-gray-600">Due Date: {dueDate}</p>
+            <button
+              onClick={closeModal}
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
