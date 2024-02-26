f6e34d2c0bdcedcddb5d3aa031e112c4f882660d
ashmit-coder <ashmitjagtap2209@gmail.com>
2024-02-26 23:59:25 +0530

    coming soon added

--- app/hackiniiitp/submit/page.tsx
+++ app/hackiniiitp/submit/page.tsx
@@ -1,6 +1,9 @@
+import { Image } from "react-bootstrap";
 const page = () => {
   return (
-    <div className="bg-bg-star bg-cover min-h-screen bg-left lg:bg-center"></div>
+    <div className="bg-bg-star bg-cover min-h-screen bg-left lg:bg-center flex  justify-center">
+      <Image src="/images/comingsoon.png" width={1000}className="py-96 mt-12" alt="Coming Soon"/>
+    </div>
   );
 };
 
