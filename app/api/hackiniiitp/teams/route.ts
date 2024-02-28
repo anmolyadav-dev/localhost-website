c3a90ea1329278f5b46b46265456b2dc1651addc
ashmit-coder <ashmitjagtap2209@gmail.com>
2024-02-28 12:56:01 +0530

    added redis client

--- app/api/hackiniiitp/teams/route.ts
+++ app/api/hackiniiitp/teams/route.ts
@@ -2,13 +2,20 @@ import { connect } from "../../dbConfig/dbConfig";
 
 import { NextRequest, NextResponse } from "next/server";
 import Team, { ITeam } from "../../models/teamModel"; // Assuming you have a Team model
+import { refreshData ,fetchData } from "../../helpers/CacheFunctions";
 
 connect();
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     // Fetch all teams from the database, excluding the password field
-    const teams: ITeam[] = await Team.find({},{password:0,_id:0,"teamMembers._id":0,"teamMembers.email":0});
-    // console.log(teams);
+    let teams:ITeam[] | null ;
+    teams = await fetchData();
+
+    if(!teams){
+     teams = await Team.find({},{password:0,_id:0,"teamMembers._id":0,"teamMembers.email":0});
+     refreshData(teams);
+    }
+
     // Return the list of teams in the response
     return NextResponse.json({
       teams,
